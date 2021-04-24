136fcc29efbf44239829bbacda2183a8a2cf7e38
Andrea Prisciantelli <priscia99@gmail.com>
2021-04-24 17:30:11 +0200

    Linked GoToHomePage servlet with Home.html

--- TIW-Project/src/it/polimi/tiw/project/controllers/GoToHomePage.java
+++ TIW-Project/src/it/polimi/tiw/project/controllers/GoToHomePage.java
@@ -1,6 +1,10 @@
 package it.polimi.tiw.project.controllers;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,55 +12,64 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import it.polimi.tiw.project.beans.User;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-/**
- * Servlet implementation class GoToHomePage
- */
+import it.polimi.tiw.project.beans.User;
+import it.polimi.tiw.project.utils.ConnectionHandler;
+
 @WebServlet("/Home")
 public class GoToHomePage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	private Connection connection = null;
 
-    public GoToHomePage() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public GoToHomePage() {
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		User user = null;
-		// Check if user is logged in; if not, then re-direct him to login page
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+		connection = ConnectionHandler.getConnection(getServletContext());
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		String loginpath = getServletContext().getContextPath() + "/index.html";
 		HttpSession session = request.getSession();
 		if (session.isNew() || session.getAttribute("user") == null) {
-			String loginpath = getServletContext().getContextPath() + "/index.html";
 			response.sendRedirect(loginpath);
 			return;
 		}
-		
-		try {
-			user = (User) session.getAttribute("user");
-		}catch(Exception e) {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "General error");
-		}
-		
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().append("\n\nThis is the Main Page\n\n");
-		response.getWriter().append("Waiting for Il Pizza to create the main page template\n");
-		response.getWriter().append("I wish it will not be too much rounded\n");
-		response.getWriter().append("Anyway, just few information to check that il server non abbia scazzato:\n\n");
-		response.getWriter().append("Username: ").append(user.getUsername());
-		response.getWriter().append("\nName: ").append(user.getName());
-		response.getWriter().append("\nSurname: ").append(user.getSurname());
-		response.getWriter().append("\nAddress: ").append(user.getAddress());
-		
-	
+		User user = (User) session.getAttribute("user");
+
+		// Redirect to the Home page and add missions to the parameters
+		String path = "/WEB-INF/Home.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext context = new WebContext(request, response, servletContext, request.getLocale());
+		context.setVariable("user", user);
+		templateEngine.process(path, context, response.getWriter());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
-	
+
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
