7983e4520c7403a38ef38be169c71b38d7446d41
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-05-05 16:17:58 +0200

    Updated DAOs

--- TIW-Project/src/it/polimi/tiw/project/dao/UserDAO.java
+++ TIW-Project/src/it/polimi/tiw/project/dao/UserDAO.java
@@ -18,11 +18,11 @@ public class UserDAO {
 	
 	// Return User information after trying to authenticate him
 	public User performUserLogin(String username, String password) throws SQLException {
-		String query = 	"SELECT id, username, name, surname, address FROM utente "
+		String query = 	"SELECT id_user, username, name, surname FROM user "
 						+ "WHERE username = ? AND password = ?";
 		try (PreparedStatement statement = connection.prepareStatement(query);) {
 			statement.setString(1, username);
-			statement.setString(2, password);
+			statement.setBytes(2, password.getBytes());
 			try (ResultSet result = statement.executeQuery();) {
 				if (!result.isBeforeFirst()) // no results, credential check failed
 					return null;
@@ -42,22 +42,24 @@ public class UserDAO {
 	}
 	
 	// Create a new user into database
-	public void createUser(String username, String password, String name, String surname, String address)
+	public void createUser(String username, String password, String name, String surname, String email, String addressTown, String addressStreet)
 	throws SQLException{
-		String query = "INSERT into utente (username, password, name, surname, address) VALUES(?, ?, ?, ?, ?)";
+		String query = "INSERT into user (username, password, name, surname, email, address_town, address_street) VALUES(?, ?, ?, ?, ?, ?, ?)";
 		try (PreparedStatement statement = connection.prepareStatement(query);) {
 			statement.setString(1, username);
-			statement.setString(2, password);
+			statement.setBytes(2, password.getBytes());
 			statement.setString(3, name);
 			statement.setString(4, surname);
-			statement.setString(5, address);
+			statement.setString(5, email);
+			statement.setString(6, addressTown);
+			statement.setString(7, addressStreet);
 			statement.executeUpdate();
 		}
 	}
 	
 	// Check if another user exists with the same username given by user
 	public boolean userAlreadyExists(String username) throws SQLException{
-		String query = 	"SELECT id FROM utente WHERE username = ?";
+		String query = 	"SELECT id_user FROM user WHERE username = ?";
 		try (PreparedStatement statement = connection.prepareStatement(query);) {
 			statement.setString(1, username);
 			try (ResultSet result = statement.executeQuery();) {
