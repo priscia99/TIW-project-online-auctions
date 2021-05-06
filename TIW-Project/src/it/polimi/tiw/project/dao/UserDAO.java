97951f23f1cca43ee1e8afeab24d02b784df22b3
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-05-06 18:56:26 +0200

    fixing signup

--- TIW-Project/src/it/polimi/tiw/project/dao/UserDAO.java
+++ TIW-Project/src/it/polimi/tiw/project/dao/UserDAO.java
@@ -71,7 +71,7 @@ public class UserDAO {
 			statement.setString(5, email);
 			statement.setString(6, addressTown);
 			statement.setString(7, addressStreet);
-			statement.executeQuery();
+			statement.execute();
 		}
 	}
 	
