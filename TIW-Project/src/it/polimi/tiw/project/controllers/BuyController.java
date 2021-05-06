839d3aa72766b2e728cae037ba85121f5dffe848
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-05-06 23:42:52 +0200

    added buy controller

---
+++ TIW-Project/src/it/polimi/tiw/project/controllers/BuyController.java
@@ -0,0 +1,99 @@
+package it.polimi.tiw.project.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.project.beans.User;
+import it.polimi.tiw.project.dao.AuctionDAO;
+import it.polimi.tiw.project.utils.ConnectionHandler;
+
+@WebServlet("/buy")
+public class BuyController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;				// engine to display page
+	private Connection connection = null;
+	
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();													// get servlet context
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);	// get template resolver
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();																// get template engine
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+		connection = ConnectionHandler.getConnection(getServletContext());
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		HttpSession session = request.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			String loginpath = getServletContext().getContextPath() + "/index.html";
+			response.sendRedirect(loginpath);
+			return;
+		}
+		
+		User user = (User) session.getAttribute("user");
+	
+		
+		String path = "/WEB-INF/Buy.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext context = new WebContext(request, response, servletContext, request.getLocale());
+		context.setVariable("user", user);
+		templateEngine.process(path, context, response.getWriter());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		HttpSession session = request.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			String loginpath = getServletContext().getContextPath() + "/index.html";
+			response.sendRedirect(loginpath);
+			return;
+		}
+		
+		User user = (User) session.getAttribute("user");
+		String query = StringEscapeUtils.escapeJava(request.getParameter("query"));
+		AuctionDAO actionDao = new AuctionDAO(connection);
+		
+		try {
+			actionDao.filterByArticleName(query);
+		}catch(Exception e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Cannot filter auctions");
+			return;
+		}
+		
+		String path = "/WEB-INF/Buy.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext context = new WebContext(request, response, servletContext, request.getLocale());
+		context.setVariable("user", user);
+		context.setVariable("query", query);
+		templateEngine.process(path, context, response.getWriter());
+	}
+	
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
